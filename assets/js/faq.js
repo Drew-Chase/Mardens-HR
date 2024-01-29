51f95531be2b524e65988b35627ade7bde0d8b7d
drew-chase <dcmanproductions@gmail.com>
2024-01-29 10:19:01 -0500

    Refactor FAQ page loading method
    The changes refactor the way how FAQ page loads by using fetch to get the FAQ content and using showdown to parse markdown to HTML. The output HTML replaces the old FAQ content on the page. The newly implemented method also introduces a structure for formatting the FAQ items.

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -1,7 +1,28 @@
 (async () => {
+    await loadFAQ();
 
-
-    async function loadFAQ(){
+    async function loadFAQ() {
         const url = "/assets/md/faq.md";
+        const response = await fetch(url);
+        const text = await response.text();
+        const converter = new showdown.Converter();
+        const html = $(`<div id='faq-items'>${converter.makeHtml(text)}</div>`);
+        const h1 = html.find("h1");
+        const p = html.find("p");
+        const faq = $(`<div id='faq-items' class="col"></div>`);
+
+        for (let i = 0; i < h1.length; i++) {
+            const h1Text = $(h1[i]).text();
+            const pText = $(p[i]).text();
+            const template = `
+                <details>
+                    <summary>${h1Text}</summary>
+                    <p>${pText}</p>
+                </details>
+            `
+            faq.append(template);
+        }
+        console.log(faq.html());
+        $(".faq-items").replaceWith(faq);
     }
 })();
