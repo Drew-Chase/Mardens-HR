3f80648d6c0f96af1224ca947d8027fd97d40a24
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-02-26 10:23:31 -0500

    Improve search function behavior and display logic
    The search function now runs only when data is available, and the search results are hidden by default. They are revealed when the search input is focused and hidden after 100ms when the input is blurred. The sidebar is also properly closed after clicking a search result. Removed some unused code as well.

--- assets/js/search.js
+++ assets/js/search.js
@@ -7,25 +7,34 @@
  * @property {Array<string>} keywords - An array of keywords associated with the data.
  */
 let data = [];
-if (data === []) {
-    $("input#search").hide();
-}
-search();
+const searchResults = $(".search-results");
+searchResults.hide();
+$("input#search").on("focus", e => {
+    searchResults.show();
+});
 
-$.get("/page-contents.json").then(response => data = response);
+$("input#search").on("blur", e => {
+    setTimeout(() => {
+        searchResults.hide();
+    }, 100)
+});
+
+$.get("/page-contents.json").then(response => data = response).then(() => {
+    if (data === []) {
+        $("input#search").hide();
+    } else {
+        search();
+    }
+});
 $("input#search").on("keyup", e => {
     if (e.key === "Enter") {
         $("#hamburger").trigger("click");
     }
     search();
 })
-$("input#search").on("blur", e => {
-    search();
-})
 
 function search() {
     const result = fuzzySearchData();
-    const searchResults = $(".search-results");
     searchResults.empty();
     if (result.length === 0) {
         searchResults.append("<p>No results found</p>");
@@ -36,7 +45,7 @@ function search() {
         const link = $(`<a class="result" href="#${item.parentElementId}">${item.displayName}</a>`);
         searchResults.append(link);
         link.on("click", () => {
-            if (window.screen.width < 1150) $("#hamburger").trigger("click");
+            closeSidebar();
             $("html, body").animate({
                 scrollTop: parentElement.offset().top - 100
             }, 0);
@@ -60,28 +69,3 @@ function fuzzySearchData() {
         return item.keywords.some(keyword => regex.test(keyword.toLowerCase()));
     });
 }
-
-// function search() {
-//     const value = $("input#search").val();
-//     if (data === []) {
-//         $("input#search").hide();
-//         return;
-//     }
-//     if (value === "") {
-//         $("*").removeClass("darken");
-//         return;
-//     }
-//     const result = data.filter(item => {
-//         return item.keywords.some(keyword => keyword.includes(value))
-//     });
-//     result.forEach(item => {
-//         const parentElement = $(`#${item.parentElementId}`);
-//         $(`#content :not("#${item.parentElementId}")`).addClass("darken");
-//         // loop through the parent element's children and remove the darken class
-//         $(`#${item.parentElementId} *`).removeClass("darken");
-//         // remove the darken class from the parent element
-//         parentElement.removeClass("darken");
-//         // loop through the parent element's parents and remove the darken class
-//         parentElement.parents().removeClass("darken");
-//     })
-// }
