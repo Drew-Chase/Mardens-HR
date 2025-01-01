8b79473dd5e7fc8045dc3c57fc5e8cd458088a52
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-01-01 16:24:24 -0500

    Add HR contact, FAQ, and form components to Home page
    Replaces placeholder content on the Home page with a structured layout, including HR contact info, hotlines, and news alerts. Adds a "Contact HR" form with input validation and a FAQ section to enhance usability and accessibility. Uses NextUI and Iconify libraries for improved UI components.

--- src/assets/pages/Home.tsx
+++ src/assets/pages/Home.tsx
@@ -1,7 +1,94 @@
+import FAQ from "../components/Home/FAQ.tsx";
+import HRNewsAlerts from "../components/Home/HRNewsAlerts.tsx";
+import HotLines from "../components/Home/HotLines.tsx";
+import HRContactInfo from "../components/Home/HRContactInfo.tsx";
+import {Button, Divider, Form, Input, Tab, Tabs, Textarea, Tooltip} from "@nextui-org/react";
+import {Icon} from "@iconify/react";
+
 export default function Home() {
     return (
-        <>
-            <h1 className="text-3xl font-bold underline">Home Page</h1>
-        </>
+        <div className={"flex flex-col mx-4 gap-8 py-8"}>
+            <div className={"flex flex-row gap-4"}>
+                <HRContactInfo/>
+                <div className={"flex flex-col gap-8 w-full"}>
+                    <HotLines/>
+                    <Divider/>
+                    <HRNewsAlerts/>
+                </div>
+            </div>
+
+            <Tabs variant={"underlined"} color={"primary"}>
+                <Tab title={"Contact HR"}>
+                    <Form className={"flex flex-col gap-4 w-full"}>
+                        <div className={"flex flex-row gap-4 grow w-full"}>
+                            <Input
+                                label={"First Name"}
+                                autoComplete={"given-name"}
+                                isRequired
+                                endContent={<Icon icon="akar-icons:person" width="24" height="24"/>}
+                                classNames={{
+                                    base: "w-full"
+                                }}
+                            />
+                            <Input
+                                label={"Last Name"}
+                                autoComplete={"family-name"}
+                                isRequired
+                                endContent={<Icon icon="fluent-mdl2:family" width="24" height="24"/>}
+                                classNames={{
+                                    base: "w-full"
+                                }}
+                            />
+                        </div>
+                        <div className={"flex flex-row gap-4 grow w-full"}>
+                            <Input
+                                label={"Email"}
+                                autoComplete={"email"}
+                                type={"email"}
+                                endContent={<Icon icon="ic:outline-email" width="24" height="24"/>}
+                                classNames={{
+                                    base: "w-full"
+                                }}
+                            />
+                            <Input
+                                label={"Phone"}
+                                autoComplete={"mobile tel"}
+                                type={"tel"}
+                                endContent={<Icon icon="lucide:phone" width="24" height="24"/>}
+                                classNames={{
+                                    base: "w-full"
+                                }}
+                            />
+                        </div>
+                        <Input
+                            label={"Subject"}
+                            autoComplete={"off"}
+                            isRequired
+                            endContent={<Icon icon="material-symbols:subject" width="24" height="24"/>}
+                            classNames={{
+                                base: "w-full"
+                            }}
+                        />
+                        <Textarea
+                            label={"Message"}
+                            isRequired
+                        />
+
+                        <Tooltip content={"Submit this form to HR for further processing."}>
+                            <Button
+                                type={"submit"}
+                                endContent={<Icon icon="proicons:send" width="24" height="24"/>}
+                                color={"primary"}
+                            >
+                                Send
+                            </Button>
+                        </Tooltip>
+                    </Form>
+                </Tab>
+
+            </Tabs>
+
+            <FAQ/>
+        </div>
     );
 }
